43b212d6c0ceeb22bec86a1813f486fe5a14016c
GuiDevloper <guicorrea384@gmail.com>
2020-09-17 18:08:07 -0300

    Add all file paths to enums

--- enums.js
+++ enums.js
@@ -6,5 +6,13 @@ exports.associations = [
 
 exports.paths = {
   guide: { path: 'src/guide' },
-  migration: { path: 'src/guide/migration' }
+  migration: { path: 'src/guide/migration' },
+  contributing: { path: 'src/guide/contributing' },
+  api: { path: 'src/api' },
+  coc: { path: 'src/coc' },
+  community: { path: 'src/community' },
+  cookbook: { path: 'src/cookbook' },
+  examples: { path: 'src/examples' },
+  'style-guide': { path: 'src/style-guide' },
+  'support-vuejs': { path: 'src/support-vuejs' }
 }
